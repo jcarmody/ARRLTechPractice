bf3afd6b3a902bc3c376b2696ff0bd76e66078b2
jcarmody <jim@jamescarmody.com>
2024-03-11 05:58:05 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace ARRL_Tech_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            QuestionHistory history = new QuestionHistory();
+            history.LoadHistory();
+
+            string path = @"C:\Users\carmodj\source\repos\ARRL Tech Practice\Questions.txt";
+
+            // Read the entire JSON file into a stream
+            using (var streamReader = new StreamReader(path))
+            {
+                var jsonDocument = JsonDocument.Parse(streamReader.ReadToEnd());
+
+                // Deserialize the JSON document into a list of MyJsonObject
+                List<Question> myObjects = new List<Question>();
+                foreach (var element in jsonDocument.RootElement.EnumerateArray())
+                {
+                    var myObject = new Question
+                    {
+                        questionRef = element.GetProperty("QuestionRef").GetString(),
+                        question = element.GetProperty("Question").GetString(),
+                        ansA = element.GetProperty("Answer_A").GetString(),
+                        ansB = element.GetProperty("Answer_B").GetString(),
+                        ansC = element.GetProperty("Answer_C").GetString(),
+                        ansD = element.GetProperty("Answer_D").GetString(),
+                        answer = element.GetProperty("Answer").GetString()
+                    };
+                    myObjects.Add(myObject);
+                }
+
+                int lenQuestions = myObjects.Count;
+                string myAnswer = "";
+                List<string> correctAnswers = new List<string>();
+                List<string> inCorrectAnswers = new List<string>();
+                do
+                {
+                    int questionIndex = random.Next(lenQuestions);
+                    Question obj = myObjects[questionIndex];
+                    Console.WriteLine(obj.questionRef);
+                    Console.WriteLine(obj.question);
+                    Console.WriteLine(obj.ansA);
+                    Console.WriteLine(obj.ansB);
+                    Console.WriteLine(obj.ansC);
+                    Console.WriteLine(obj.ansD);
+                    Console.WriteLine();
+                    Console.WriteLine("Enter \"s\" to skip this question.");
+
+                    myAnswer = Console.ReadLine().ToUpper();
+                    if (myAnswer != "S")
+                    {
+                        if (myAnswer == obj.answer)
+                        {
+                            history.AddCorrect(obj);
+                        }
+                        else
+                        {
+                            history.AddInCorrect(obj);
+                        }
+                        myObjects.RemoveAt(questionIndex); // so we don't see it again this session
+
+                        Console.WriteLine("\nHit <enter> to continue... ");
+                        Console.WriteLine("Enter \"e\" to end this practice session.");
+                        myAnswer = Console.ReadLine().ToUpper();
+                    }
+                    else
+                    {
+                        history.AddSkipped(obj);
+                    }
+
+                    Console.Clear();
+                } while (myAnswer != "E");
+
+                history.DisplayHistory();
+                history.SaveHistory();
+            }
+        }
+    }
+}
---
+++ QuestionHistory.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ARRL_Tech_Practice
+{
+    /// <summary>
+    /// This class collects the results of a practice session.  Questions answered correctly/incorrectly or skipped will be saved for future, focused sessions.
+    /// </summary>
+    class QuestionHistory
+    {
+        string path = @"C:\Users\carmodj\source\repos\ARRL Tech Practice\Answered.txt";
+
+        private List<string> correctAnswers = new List<string>();
+        private List<string> inCorrectAnswers = new List<string>();
+        private List<string> skippedQuestions = new List<string>();
+
+        public void AddCorrect(Question question)
+        {
+            correctAnswers.Add(question.questionRef);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("correct");
+            Console.ResetColor();
+        }
+        public void AddCorrect(string question)
+        {
+            correctAnswers.Add(question);
+        }
+        public void AddInCorrect(Question question)
+        {
+            inCorrectAnswers.Add(question.questionRef);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"incorrect ({question.answer})");
+            Console.ResetColor();
+        }
+        public void AddInCorrect(string question)
+        {
+            inCorrectAnswers.Add(question);
+        }
+        public void AddSkipped(Question question)
+        {
+            skippedQuestions.Add(question.questionRef);
+        }
+        public void AddSkipped(string question)
+        {
+            skippedQuestions.Add(question);
+        }
+
+
+
+        public void SaveHistory()
+        {
+            List<string> fileData = new List<string>();
+            fileData.Add("Correct Answers");
+            fileData.AddRange(correctAnswers);
+            fileData.Add("Incorrect Answers");
+            fileData.AddRange(inCorrectAnswers);
+            fileData.Add("Skipped Questions");
+            fileData.AddRange(skippedQuestions);
+
+            File.WriteAllLines(path, fileData.ToArray());
+        }
+        public void LoadHistory()
+        {
+            string[] historyQuestions = File.ReadAllLines(path);
+            string typeOfAnswer = "";
+
+            foreach (string histQ in historyQuestions)
+            {
+                switch(histQ)
+                {
+                    case "Correct Answers":
+                        typeOfAnswer = "correct";
+                        continue;
+                    case "Incorrect Answers":
+                        typeOfAnswer = "incorrect";
+                        continue;
+                    case "Skipped Questions":
+                        typeOfAnswer = "skipped";
+                        continue;
+                }
+                    
+                switch(typeOfAnswer)
+                {
+                    case ("correct"):
+                        AddCorrect(histQ);
+                        break;
+                    case ("incorrect"):
+                        AddInCorrect(histQ);
+                        break;
+                    case ("skipped"):
+                        AddSkipped(histQ);
+                        break;
+                }
+            }
+        }
+        public void DisplayHistory()
+        {
+            Console.WriteLine("Correct Answers");
+            foreach (string qRef in correctAnswers)
+            {
+                Console.WriteLine(qRef);
+            }
+            Console.WriteLine("\nIncorrect Answers");
+            foreach (string qRef in inCorrectAnswers)
+            {
+                Console.WriteLine(qRef);
+            }
+            Console.WriteLine("\nSkipped Questions");
+            foreach (string qRef in skippedQuestions)
+            {
+                Console.WriteLine(qRef);
+            }
+        }
+        
+    }
+
+    /// <summary>
+    /// This class is the structure of a practice question
+    /// </summary>
+    class Question
+    {
+        public string questionRef { get; set; }
+        public string question { get; set; }
+        public string ansA { get; set; }
+        public string ansB { get; set; }
+        public string ansC { get; set; }
+        public string ansD { get; set; }
+        public string answer { get; set; }
+    }
+}
